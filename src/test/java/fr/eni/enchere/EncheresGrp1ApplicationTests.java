e63fd3c84818e4c6ed8da86668a1255e3cc11cd6
Alan Lens <Allan18040@gmail.com>
2025-02-24 15:00:52 +0100

    Creation du projet

---
+++ src/test/java/fr/eni/enchere/EncheresGrp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.enchere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncheresGrp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
